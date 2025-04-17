d4f191f007341daf7b38bbb8a0c6dfec4539ebe4
John Children <john.children@quantinuum.com>
2025-04-17 09:12:35 +0100

    chore: Add CI and release steps

--- devenv.nix
+++ devenv.nix
@@ -3,11 +3,11 @@ let
   qulacs = pkgs.callPackage ./nix/qulacs.nix {};
 in {
   env = {
-    LIBQULACS_PATH = "${qulacs}/lib";
+    QULACS_PATH = qulacs;
     LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
   };
   
-  packages = [ pkgs.boost pkgs.eigen pkgs.mpi qulacs ] ++ [ pkgs.valgrind ];
+  packages = [ pkgs.boost pkgs.eigen pkgs.mpi qulacs pkgs.release-plz pkgs.cargo-semver-checks ];
   languages.rust.enable = true;
   languages.nix.enable = true;
 }
